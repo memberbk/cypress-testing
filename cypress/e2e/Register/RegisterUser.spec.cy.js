cd7e16ba46a93f7e0546214e13a462c8ae665393
memberbb <remember.bk26@gmail.com>
2024-08-25 22:52:31 +0700

    for run case failed

--- cypress/e2e/Register/RegisterUser.spec.cy.js
+++ cypress/e2e/Register/RegisterUser.spec.cy.js
@@ -8,7 +8,7 @@ describe("Register User", () => {
     it("Register Uaer", () => {
 
       cy.get('a').contains(' Signup / Login').should('be.visible').click()
-      cy.get('input[data-qa="signup-name"]').type('Test User')
+      cy.get('input[data-qa="signup"]').type('Test User')
       cy.get('input[data-qa="signup-email"]').type('EmailTestUser@mail.com')
       cy.get('button[data-qa="signup-button"]').click()
       cy.get('#id_gender1').click()
