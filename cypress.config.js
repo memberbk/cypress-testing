5e400eed95d59c3e1718b9b9793c4390e5393b4c
memberbb <remember.bk26@gmail.com>
2024-08-25 23:39:29 +0700

    add     video: true, // ตั้งค่าเป็น true เพื่อให้บันทึกวิดีโอ

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: true, // ตั้งค่าเป็น true เพื่อให้บันทึกวิดีโอ
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
