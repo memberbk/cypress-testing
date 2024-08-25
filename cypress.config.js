fd072310f9da8364f2a0f2a8d36859be0254c208
memberbb <remember.bk26@gmail.com>
2024-08-25 23:46:36 +0700

    add screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     video: true, // ตั้งค่าเป็น true เพื่อให้บันทึกวิดีโอ
+    screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
